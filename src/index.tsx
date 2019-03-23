85b244e0ea77473bb60746eb5e31e443e4c9cf8b
Stefan Mansson <stefan.aotik@gmail.com>
2019-03-23 09:46:28 +0200

    feat: mdx support

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MDXDocument from './test.mdx';
 
-ReactDOM.render(<p>Hi</p>, document.getElementById('root'));
+ReactDOM.render(
+    <>
+        <MDXDocument />
+    </>,
+    document.getElementById('root')
+);
